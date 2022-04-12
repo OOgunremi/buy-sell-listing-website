92cb75b8422b48f4a6dd8e5a10231e72c64fc36c
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-12 09:40:22 -0600

    add post route to users.js

--- routes/users.js
+++ routes/users.js
@@ -23,5 +23,43 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post("/", (req, res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const username = req.body.username;
+    const email =  req.body.email;
+    const password = req.body.password;
+    const street = req.body.street;
+    const city = req.body.city;
+    const province = req.body.province;
+    const postCode = req.body.post_code;
+    const admin = req.body.admin;
+
+    const values = [`${firstName}`, `${lastName}`, `${username}`, `${email}`, `${password}`, `${admin}`, `${street}`, `${city}`, `${province}`, `${postCode}`];
+    const queryString = `INSERT INTO users (first_name, last_name, username, email, password, street, city, province, post_code, admin)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  RETURNING *;`;
+    return pool
+      .query(queryString, values)
+      .then((result) => {
+        console.log(result.rows[0]);
+        return result.rows[0];
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+
+  });
+
+
+
+
+
+
+
+
   return router;
 };
+
+
