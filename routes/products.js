767cd7fed6fc5276014cb35a7b0825a6432b73ed
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-12 15:07:36 -0600

    modify server routes

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  //Browse
   router.get("/", (req, res) => {
     let query = `SELECT * FROM products`;
     console.log(query);
@@ -16,6 +17,76 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  //Read
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    let query = `SELECT * FROM products WHERE id = ${id}`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const products = data.rows;
+        res.json({ products });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  });
+
+  //Edit
+  router.post("/:id", (req, res) => {
+
+
+
+
+  });
+
+
+  //A
+
+  router.post("/", (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const sellerId = req.body.seller_id;
+    const email =  req.body.email;
+    const description = req.body.description;
+    const category = req.body.category;
+    const imageUrlOne = req.body.image_url_one;
+    const available = req.body.available;
+    const postCode = req.body.post_code;
+    const sold = req.body.sold;
+
+    const values = [`${name}`, `${price}`, `${sellerId}`, `${email}`, `${description}`, `${category}`, `${imageUrlOne}`, `${available}`, `${postCode}`, `${sold}`];
+    const queryString = `INSERT INTO products (name, price, seller_id, description, category, image_url_one, available, sold)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8)  RETURNING *;`;
+    db
+      .query(queryString, values)
+      .then((result) => {
+
+
+
+        console.log(result.rows[0]);
+        return result;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+  });
+
+
+  //D
+  router.post("/:id/delete", (req, res) => {
+    const id = req.params.id;
+
+
+  });
+
+
+
   return router;
 };
 
--- routes/users.js
+++ routes/users.js
@@ -9,56 +9,10 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM users`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.get("/login/:id", (req, res) => {
+    res.cookie('user_id', req.params.id);
+    res.redirect('/');
   });
-
-  router.post("/", (req, res) => {
-    const firstName = req.body.first_name;
-    const lastName = req.body.last_name;
-    const username = req.body.username;
-    const email =  req.body.email;
-    const password = req.body.password;
-    const street = req.body.street;
-    const city = req.body.city;
-    const province = req.body.province;
-    const postCode = req.body.post_code;
-    const admin = req.body.admin;
-
-    const values = [`${firstName}`, `${lastName}`, `${username}`, `${email}`, `${password}`, `${admin}`, `${street}`, `${city}`, `${province}`, `${postCode}`];
-    const queryString = `INSERT INTO users (first_name, last_name, username, email, password, street, city, province, post_code, admin)
-  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  RETURNING *;`;
-    return pool
-      .query(queryString, values)
-      .then((result) => {
-        console.log(result.rows[0]);
-        return result.rows[0];
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-
-
-  });
-
-
-
-
-
-
-
-
   return router;
 };
 
