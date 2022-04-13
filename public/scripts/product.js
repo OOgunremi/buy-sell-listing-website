5c412c779fd776aa6156b89d3d6a941a360634df
Kurt Maglalang <kbm126@live.com>
2022-04-13 22:43:35 +0000

    user can now favourite the product or remove product from favourites

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -1,66 +1,43 @@
 // user viewed product page jquery functions
 
 $(() => {
-
   // favorite button pressed
-  // ! tags to be updated once that is completed
   $('.product-page .fav-button').on('click',onFavoriteButtonClick);
-
 });
 
 const onFavoriteButtonClick = function(e) {
-  alert('still in development');
-  e.preventDefault(); // ! this might not be necessary
+  // alert('still in development');  // ! temp
 
-  // ! check the status of the favorite button - this might be from the server or from the web page
+  // check if there is a cookie before adding to favourites
+  if (!document.cookie.length) {
+    alert('please login to add to your favourites');
+    return;
+  }
 
-  /*
-    todo check the status of this favorite from teh database
-      * give the user id, check if the product id exists, return the response to the client and decide from there
-    todo add this product to your favorites
-    todo remove this product fromk your favorites
-  */
-  // todo may need to add a check if there is a cookie to begin with
-  const buyerID = document.cookie.split('=')[1]; // ! this may change out depending if cookie parser is installed
+  // ! this may change out depending if cookie parser is installed
+  const buyerID = document.cookie.split('=')[1];
   const $productId = $('.product-page').attr('id');
 
-  $.get(`/favourites?user_id=${buyerID}&product_id=${$productId}`)
+  $.get(`/favourites`, {user_id: buyerID, product_id: $productId})
     .then((data)=> {
-      console.log(data.favourites);
-
+      // check if favourite already exists
       if (!data.favourites.length) {
-        console.log('in favourties');
         $.post(`/favourites`, {user_id: buyerID, product_id: $productId})
-          .then()
+          .then((data)=> {
+            alert('added to your favourites');
+            console.log('added to your favourites');
+          })
           .catch(error => console.log(error));
         return;
       }
 
-      console.log("🚀 ~ file: product.js ~ line 40 ~ .then ~ data.favourites[0];", data.favourites[0]);
-
-      // // todo remove this from the favourites table
-      // $.post(`/favourites`)
-      //   .then()
-      //   .catch(error => console.log(error));
+      // remove product from the favourites table
+      $.post(`/favourites/${data.favourites[0].id}/delete`)
+        .then((data)=> {
+          alert('removed from your favourites');
+          console.log('removed from your favourites');
+        })
+        .catch(error => console.log(error));
     })
     .catch(error => console.log(error));
 };
-
-// const onContactSellerButtonClick = function(e) {
-//   // check if the text area is empty
-//   // ! tags to be updated once that is completed
-//   const $contactSellerText = $('.contact-seller .contact-message').val();
-//   if (!$contactSellerText) {
-//     alert('contact seller field must not be empty');  // ! this may change later on
-//     return;
-//   }
-
-//   const $productId = $('.product-page').attr('id');
-//   const $sellerId = $('.product-page .contact-seller').attr('id');
-//   const buyerID = document.cookie.split('=');
-
-//   // send the data to server
-//   $.post('/messages', {product_id: $productId, seller_id: $sellerId, buyer_id: buyerID[1], messages: $contactSellerText}).then(function() {
-//     // todo clear out the text area field
-//   });
-// };
--- routes/favourites.js
+++ routes/favourites.js
@@ -3,9 +3,11 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM favourites`;
-    console.log(query);
-    db.query(query)
+    let query = `SELECT * FROM favourites WHERE user_id=$1 and product_id=$2`;
+    // console.log('server side', query);
+    // console.log('query values', req.query);
+
+    db.query(query,[req.query.user_id, req.query.product_id])
       .then(data => {
         const favourites = data.rows;
         res.json({ favourites });
@@ -16,5 +18,50 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  //Add
+  router.post("/", (req, res) => {
+    // console.log('server side', req.body);
+
+    const values = [req.body.product_id, req.body.user_id];
+    const queryString = `
+    INSERT INTO favourites (product_id,  user_id)
+    VALUES($1, $2)  RETURNING *;
+    `;
+
+    db.query(queryString, values)
+      .then((result) => {
+        // console.log('add', result.rows[0]);
+        res.send(result.rows[0]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // delete
+  router.post("/:id/delete", (req, res) => {
+    const id = req.params.id;
+
+    const values = [`${id}`];
+    const queryString = `
+    DELETE FROM favourites
+    WHERE id=$1
+    RETURNING *;
+    `;
+    // console.log('value', values);
+    // console.log('queryString',  queryString);
+
+    db.query(queryString, values)
+      .then((result) => {
+        // console.log('delete', result.rows[0]);
+        res.send(result.rows[0]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+
   return router;
 };
