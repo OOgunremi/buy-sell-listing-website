e45d5ca2706653e8899edc1384b1a1ff1c5999ce
Kurt Maglalang <kbm126@live.com>
2022-04-13 21:34:46 +0000

    early implementation of the favourtie product function

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -1,49 +1,66 @@
-// uver viewed product page jquery functions
+// user viewed product page jquery functions
 
 $(() => {
-  /*
-    // todo monitor send button pressed
-    // todo monitor favorite button pressed
-    todo send the text data to the server
-    todo toggle the favorite button and send the information to the server
-  */
-
-
-  // send button pressed
-  // ! tags to be updated once that is completed
-  $('.contact-seller .send').on('click', onContactSellerButtonClick);
 
   // favorite button pressed
   // ! tags to be updated once that is completed
-  $('.product-details .favorite').on('click',onFavoriteButtonClick);
+  $('.product-page .fav-button').on('click',onFavoriteButtonClick);
 
 });
-
-
-
 
 const onFavoriteButtonClick = function(e) {
   alert('still in development');
   e.preventDefault(); // ! this might not be necessary
 
   // ! check the status of the favorite button - this might be from the server or from the web page
-  // todo depnding on the previous status, send a message to the server to either add or delete favorite
+
+  /*
+    todo check the status of this favorite from teh database
+      * give the user id, check if the product id exists, return the response to the client and decide from there
+    todo add this product to your favorites
+    todo remove this product fromk your favorites
+  */
+  // todo may need to add a check if there is a cookie to begin with
+  const buyerID = document.cookie.split('=')[1]; // ! this may change out depending if cookie parser is installed
+  const $productId = $('.product-page').attr('id');
+
+  $.get(`/favourites?user_id=${buyerID}&product_id=${$productId}`)
+    .then((data)=> {
+      console.log(data.favourites);
+
+      if (!data.favourites.length) {
+        console.log('in favourties');
+        $.post(`/favourites`, {user_id: buyerID, product_id: $productId})
+          .then()
+          .catch(error => console.log(error));
+        return;
+      }
+
+      console.log("🚀 ~ file: product.js ~ line 40 ~ .then ~ data.favourites[0];", data.favourites[0]);
+
+      // // todo remove this from the favourites table
+      // $.post(`/favourites`)
+      //   .then()
+      //   .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 };
 
-const onContactSellerButtonClick = function(e) {
-  alert('still in development');
-  e.preventDefault();
+// const onContactSellerButtonClick = function(e) {
+//   // check if the text area is empty
+//   // ! tags to be updated once that is completed
+//   const $contactSellerText = $('.contact-seller .contact-message').val();
+//   if (!$contactSellerText) {
+//     alert('contact seller field must not be empty');  // ! this may change later on
+//     return;
+//   }
 
-  // check if the text area is empty
-  // ! tags to be updated once that is completed
-  const $contactSellerText = $('.contactSellerText').val();
-  if (!$contactSellerText) {
-    alert('contact seller field must not be empty');
-    return;
-  }
+//   const $productId = $('.product-page').attr('id');
+//   const $sellerId = $('.product-page .contact-seller').attr('id');
+//   const buyerID = document.cookie.split('=');
 
-  // send the data to server
-  $.post('/message', $contactSellerText.serialize()).then(function() {
-    $contactSellerText.val(''); // ! this might not be necessary
-  });
-};
+//   // send the data to server
+//   $.post('/messages', {product_id: $productId, seller_id: $sellerId, buyer_id: buyerID[1], messages: $contactSellerText}).then(function() {
+//     // todo clear out the text area field
+//   });
+// };
