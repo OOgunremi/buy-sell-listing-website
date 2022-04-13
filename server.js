eca168f506c5c8d544ad533e6ed009e59b13d1c5
Lucas Tan <sheng.x.tan@gmail.com>
2022-04-13 19:13:33 +0000

    create basic css for message.ejs and product.ejs

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/favourites", favouritesRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("message");
 });
 
 app.listen(PORT, () => {
