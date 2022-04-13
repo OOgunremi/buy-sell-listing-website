ba4b68d1ca7a97791d85f035b587cd024ffeae9e
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-13 15:33:02 -0600

    build messaging function

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,46 +1,92 @@
 // messages page jquery functions
 
-$(() => {
-  /*
-    // todo monitor if a conversation was selected
-    // todo monitor send button to send message data back to the seller
-    todo clear out the screen
-    todo get the conversation messages
-    todo setinterval update the conversation messsages
-      ! POTENTIAL PROBLEM - need to make sure that the set interval stops when the user leaves this page
-  */
+// $(() => {
+//   /*
+//     // todo monitor if a conversation was selected
+//     // todo monitor send button to send message data back to the seller
+//     todo clear out the screen
+//     todo get the conversation messages
+//     todo setinterval update the conversation messsages
+//       ! POTENTIAL PROBLEM - need to make sure that the set interval stops when the user leaves this page
+//   */
 
-  // ! tags to be updated once that is completed
-  $('.conversation').on('click',onConversationClick);
-  // ! tags to be updated once that is completed
-  $('.conversationSend').on('click', onConversationSendClick);
+//   // ! tags to be updated once that is completed
+//   $('.conversation').on('click',onConversationClick);
+//   // ! tags to be updated once that is completed
+//   $('.conversationSend').on('click', onConversationSendClick);
+// });
+
+let renderMessages = function (messages) {
+  //resets the elements in the messages container
+  let messagesContainer = $('.messages').html('');
+
+  //loops through all the array of messages and prepends HTML formated messages for chronological display
+  for (let message of messages) {
+    let messageHTML = createMessageElement(message);
+    messagesContainer.a(messageHTML);
+  }
+};
+
+//protects against XSS Hacks
+const escapeHacks = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createMessageElement = function(message) {
+  let messageHTML = `<article class="message-article">
+  <header class="article-header">
+    <div class="profile-name-avatar">
+      <span>${messages.product_id}</span>
+    </div>
+    <div class="messages-header">
+    ${messages.buyer_id}
+    </div>
+  </header>
+  <div class="tweets-middle">${escapeHacks(tweet.content.text)}</div>
+  <footer class="article-footer">
+    <div class="days-counter">${timeago.format(message.created_at)}</div>
+
+  </footer>
+</article>`;
+  return messageHTML;
+
+};
+
+//waits for the page to fully load before calling the callback
+$(document).ready(function() {
+
+  //This is the submit handler
+  $("#message-form").submit(function(event) {
+    //prevents route redirection upon submission
+    event.preventDefault();
+
+    //grabs tweet values excluding spaces at begining and the end
+    let tweetValue = $("#message-text").val().trim();
+    //checks if no character was typed or if excess was typed, returns error messages accordingly
+    if (!tweetValue) {
+
+      // POST request
+      const url = '/messages/';
+      const data = $(this).serialize();
+      $.ajax({
+        url: "/messages",
+        type: "post",
+        data: data
+      }).then((data) => {
+        //loads tweet GET request automatically without browser refresh
+        loadMessages();
+      });
+    }
+  });
+
+  // GET request function
+  const loadMessages = function() {
+    $.get('/messages', function(data, status) {
+      // console.log(data);
+      renderMessages(data);
+    });
+  };
+  loadMessages();
 });
-
-const createMessageElement = function() {
-
-};
-
-const renderMessages = function() {
-
-};
-
-const loadMessages = function() {
-
-};
-
-const onConversationClick = function(e) {
-  alert('still in development');
-  e.preventDefault(); // ! this might not be necessary
-
-  // clear container
-  $('.conversation-container').empty();
-
-  // render messages
-};
-
-const onConversationSendClick = function(e) {
-  alert('still in development');
-  e.preventDefault(); // ! this might not be necessary
-
-
-};
--- routes/messages.js
+++ routes/messages.js
@@ -14,6 +14,7 @@ module.exports = (db) => {
     console.log(query);
     db.query(query)
       .then(data => {
+        // console.log(data);
         const messages = data.rows;
         res.json({ messages });
       })
@@ -23,5 +24,44 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    let query = `SELECT * FROM messages WHERE id = ${id}  AND messages.buyer_id = 1
+    AND messages.seller_id = 2`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const messages = data.rows;
+        res.json({ messages });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  });
+
+  router.post("/", (req, res) => {
+    const messages = req.body.messages;
+
+
+    const values = [`${messages}`];
+    const queryString = `INSERT INTO products (name, price, seller_id, description, category, image_url_one, available, sold)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8)  RETURNING *;`;
+    db
+      .query(queryString, values)
+      .then((result) => {
+
+        console.log(result.rows[0]);
+        return result;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+  });
+
   return router;
 };
