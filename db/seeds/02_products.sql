700bb19f415d8826bb8db1eadeac2e769524b636
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-12 20:33:45 -0600

    update database with more random data. add min max to products home page

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,43 +1,17 @@
-INSERT INTO products (
-  name,
-  price,
-  seller_id,
-  description,
-  category,
-  image_url_one,
-  available,
-  sold
-)
-
-VALUES(
-  'Mountain Bike',
-  100,
-  01,
-  'A blue mountain bike',
-  'Sports',
+INSERT INTO products (name, price, seller_id, description, image_url_one, available, sold) VALUES ('Mountain Bike', 100, 01, 'A blue mountain bike',
  'https://www.carousell.ph/p/specialized-blue-mountain-bike-201091807/',
-  true,
-  false
-);
+  true, false);
 
-INSERT INTO products (
-  name,
-  price,
-  seller_id,
-  description,
-  category,
-  image_url_one,
-  available,
-  sold
-)
+INSERT INTO products (name, price, seller_id, description, image_url_one, available, sold) VALUES ('Coffee Cup', 200, 02, 'A brown cup for sale',
+ 'https://www.barry-callebaut.com/en-US/artisans/mona-lisa/products//CHD-CP-90099', true, false);
 
-VALUES(
-  'Coffee Cup',
-  200,
-  02,
-  'A brown cup for sale',
-  'Household Items',
- 'https://www.barry-callebaut.com/en-US/artisans/mona-lisa/products//CHD-CP-90099',
-  true,
-  false
-);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Fond - Neutral', 237, 2, 'hac habitasse platea dictumst etiam faucibus cursus urna', 'http://dummyimage.com/219x100.png/ff4444/ffffff', false, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Wine - Jaboulet Cotes Du Rhone', 233, 1, 'in tempor turpis nec euismod scelerisque quam', 'http://dummyimage.com/114x100.png/cc0000/ffffff', true, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Wine - Guy Sage Touraine', 257, 2, 'sem mauris laoreet ut rhoncus aliquet pulvinar', 'http://dummyimage.com/153x100.png/dddddd/000000', false, true);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Lettuce - Lolla Rosa', 232, 2, 'aliquet pulvinar sed nisl nunc', 'http://dummyimage.com/120x100.png/cc0000/ffffff', false, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Sprouts - Alfalfa', 288, 1, 'nec nisi volutpat eleifend donec ut', 'http://dummyimage.com/210x100.png/cc0000/ffffff', false, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Potatoes - Pei 10 Oz', 175, 1, 'id mauris vulputate elementum nullam varius nulla', 'http://dummyimage.com/198x100.png/5fa2dd/ffffff', false, true);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('V8 - Tropical Blend', 118, 1, 'ornare imperdiet sapien urna pretium nisl ut volutpat', 'http://dummyimage.com/180x100.png/dddddd/000000', true, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Soap - Hand Soap', 242, 2, 'quis odio consequat varius integer ac leo pellentesque ultrices mattis', 'http://dummyimage.com/147x100.png/dddddd/000000', false, false);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('French Pastry - Mini Chocolate', 8, 1, 'condimentum curabitur in libero ut massa', 'http://dummyimage.com/127x100.png/dddddd/000000', false, true);
+INSERT INTO products (name, price, seller_id, description,   image_url_one, available, sold) VALUES ('Extract - Almond', 225, 1, 'ipsum dolor sit amet consectetuer adipiscing elit proin', 'http://dummyimage.com/186x100.png/5fa2dd/ffffff', false, false);
